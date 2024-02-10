e3e8aa15852167ec635e8312ac4ff75223bea926
benardidoko <benardidoko@yahoo.com>
2024-02-10 01:54:38 +0000

    python

---
+++ models/city.py
@@ -0,0 +1,9 @@
+
+ntains the City model"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Implements the City class"""
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+
+ntains the Review model"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Implements the Review model"""
+    place_id = ""
+    user_id = ""
+    text = ""
